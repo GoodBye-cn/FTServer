8a8929fbe745ae42929d4fd31f357a647a9e470d
Goodbye <guwei9911@outlook.com>
2023-04-09 23:05:52 +0800

    update

--- inc/Worker.h
+++ inc/Worker.h
@@ -2,6 +2,7 @@
 #define WORKER_H
 
 #include <event2/bufferevent.h>
+#include "Format.h"
 
 #ifndef BUFFLEN
 #define BUFFLEN 2048
@@ -12,19 +13,24 @@ class Handler;
 
 class Worker {
 public:
+    enum Status { PARSE, OPENFILE, SEND };
+    enum Line_Status { LINE_OK, LINE_OPEN, LINE_BAD };
+
+public:
     Worker(/* args */);
     ~Worker();
     void process();
     void set_handler(Handler* handler);
     void write_to_buff(char* data, size_t size);
-public:
-    enum State { PARSE, OPENFILE, SEND };
+    Worker::Line_Status parse_request();
 
 private:
     Handler* handler;
     char buff[BUFFLEN]; /* 存放数据的buff */
     int buff_size;      /* 当前buff中的数据大小 */
-    State state;
+    Status status;
+    Request request;
+    char* path;
 };
 
 #endif
--- src/Worker.cc
+++ src/Worker.cc
@@ -2,7 +2,7 @@
 #include "Handler.h"
 
 Worker::Worker() {
-    this->state = PARSE;
+    this->status = PARSE;
 }
 
 Worker::~Worker() {}
@@ -16,21 +16,29 @@ Worker::~Worker() {}
  * STATA: PARSE OPENFILE SEND
  */
 void Worker::process() {
-    switch (state) {
-    case PARSE:
-        /* 解析路径 */
-        break;
-    case OPENFILE:
-        /* 打开文件 */
-        /* 设置报头 */
-        /* 发送报头 */
-        /* 设置send_over为false */
-        break;
-    case SEND:
-        /* 读取数据，调用Handler的write_data函数，直接放到bufferevent中，如果文件发送完，设置send_over为true */
-        break;
-    default:
-        break;
+    Line_Status line_state = LINE_OK;
+    while (true) {
+        switch (status) {
+        case PARSE:
+            /* 解析路径 */
+            line_state = parse_request();
+            if (line_state == LINE_OK) {
+                status = OPENFILE;
+            }
+            return;
+            break;
+        case OPENFILE:
+            /* 打开文件 */
+            /* 设置报头 */
+            /* 发送报头 */
+            /* 设置send_over为false */
+            break;
+        case SEND:
+            /* 读取数据，调用Handler的write_data函数，直接放到bufferevent中，如果文件发送完，设置send_over为true */
+            break;
+        default:
+            break;
+        }
     }
 }
 
@@ -41,4 +49,17 @@ void Worker::set_handler(Handler* handler) {
 void Worker::write_to_buff(char* data, size_t size) {
     memcpy(buff + buff_size, data, size);
     buff_size += size;
+}
+
+Worker::Line_Status Worker::parse_request() {
+    if (buff_size > sizeof(int)) {
+        memcpy(&request, buff, sizeof(int));
+        /* 请求读取完毕，可以获取路径 */
+        if (request.length + sizeof(int) == buff_size) {
+            path = request.path;
+            return LINE_OK;
+        }
+        return LINE_OPEN;
+    }
+    return LINE_OPEN;
 }
